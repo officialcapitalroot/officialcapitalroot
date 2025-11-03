b8ec88b90c5bf5e1574be0771043cf4ca4cf7fda
ron131977 <officialcapitalroot@gmail.com>
2025-11-04 05:07:44 +0530

    KICK ON

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,61 @@
+// import '../styles/globals.css'
+// import Head from 'next/head'
+// import { useEffect } from 'react'
+
+// export default function App({ Component, pageProps }) {
+//   useEffect(() => {
+//     // Load YouTube IFrame API
+//     const tag = document.createElement('script')
+//     tag.src = "https://www.youtube.com/iframe_api"
+//     const firstScriptTag = document.getElementsByTagName('script')[0]
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+//   }, [])
+
+//   return (
+//     <>
+//       <Head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+//         <meta name="format-detection" content="telephone=no" />
+//       </Head>
+//       <Component {...pageProps} />
+//     </>
+//   )
+// }
+
+
+// import '../styles/globals.css'
+// import Head from 'next/head'
+// import { useEffect } from 'react'
+
+// export default function App({ Component, pageProps }) {
+//   useEffect(() => {
+//     // Load YouTube IFrame API
+//     const tag = document.createElement('script')
+//     tag.src = "https://www.youtube.com/iframe_api"
+//     const firstScriptTag = document.getElementsByTagName('script')[0]
+//     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+//     // Load ad script
+//     const adScript = document.createElement('script')
+//     adScript.innerHTML = `(function(s){s.dataset.zone='10137789',s.src='https://forfrogadiertor.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+//     document.head.appendChild(adScript)
+//   }, [])
+
+//   return (
+//     <>
+//       <Head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+//         <meta name="format-detection" content="telephone=no" />
+//       </Head>
+//       <Component {...pageProps} />
+//     </> 
+//   )
+// }
+
+
+
+
+
 import '../styles/globals.css'
 import Head from 'next/head'
 import { useEffect } from 'react'
@@ -9,6 +67,16 @@ export default function App({ Component, pageProps }) {
     tag.src = "https://www.youtube.com/iframe_api"
     const firstScriptTag = document.getElementsByTagName('script')[0]
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+    // Load first ad script
+    const adScript1 = document.createElement('script')
+    adScript1.innerHTML = `(function(s){s.dataset.zone='10137789',s.src='https://forfrogadiertor.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+    document.head.appendChild(adScript1)
+
+    // Load second ad script
+    const adScript2 = document.createElement('script')
+    adScript2.innerHTML = `(function(s){s.dataset.zone='10137799',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+    document.head.appendChild(adScript2)
   }, [])
 
   return (
@@ -18,6 +86,6 @@ export default function App({ Component, pageProps }) {
         <meta name="format-detection" content="telephone=no" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </> 
   )
 }
