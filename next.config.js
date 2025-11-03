cdf41232a1e58892166715aa9c8cb9b06d1055d5
ron131977 <officialcapitalroot@gmail.com>
2025-11-03 12:40:17 +0530

    re do it

--- next.config.js
+++ next.config.js
@@ -1,56 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     domains: ['i.ytimg.com', 'img.youtube.com','abyss.to'],
-//     formats: ['image/webp', 'image/avif'],
-//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-//   },
-//   async headers() {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'X-Content-Type-Options',
-//             value: 'nosniff'
-//           },
-//           {
-//             key: 'X-Frame-Options',
-//             value: 'DENY'
-//           },
-//           {
-//             key: 'X-XSS-Protection',
-//             value: '1; mode=block'
-//           },
-//           {
-//             key: 'Referrer-Policy',
-//             value: 'origin-when-cross-origin'
-//           },
-//           {
-//             key: 'Strict-Transport-Security',
-//             value: 'max-age=31536000; includeSubDomains'
-//           }
-//         ],
-//       },
-//     ]
-//   },
-//   compress: true,
-//   poweredByHeader: false,
-//   generateEtags: false,
-//   trailingSlash: true,
-// }
-
-// module.exports = nextConfig
-
-
-
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -63,15 +10,6 @@ const nextConfig = {
   },
   async headers() {
     return [
-       {
-        source: '/:path*.xml',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml',
-          },
-        ],
-      },
       {
         source: '/(.*)',
         headers: [
