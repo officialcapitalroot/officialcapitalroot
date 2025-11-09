5b393ce0d0ce6e79fdb97abf7de8fefd61891750
ron131977 <officialcapitalroot@gmail.com>
2025-11-09 07:01:44 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,57 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     domains: ['i.ytimg.com', 'img.youtube.com'],
-//     formats: ['image/webp', 'image/avif'],
-//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-//   },
-//   async headers() {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'X-Content-Type-Options',
-//             value: 'nosniff'
-//           },
-//           {
-//             key: 'X-Frame-Options',
-//             value: 'DENY'
-//           },
-//           {
-//             key: 'X-XSS-Protection',
-//             value: '1; mode=block'
-//           },
-//           {
-//             key: 'Referrer-Policy',
-//             value: 'origin-when-cross-origin'
-//           },
-//           {
-//             key: 'Strict-Transport-Security',
-//             value: 'max-age=31536000; includeSubDomains'
-//           }
-//         ],
-//       },
-//     ]
-//   },
-//   compress: true,
-//   poweredByHeader: false,
-//   generateEtags: false,
-//   trailingSlash: true,
-// }
-
-// module.exports = nextConfig
-
-
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['i.ytimg.com', 'img.youtube.com'],
+    domains: ['i.ytimg.com', 'img.youtube.com', 'www.dailymotion.com', 'dmcdn.net', 'capitalroot.vercel.app', 'short.icu'],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
